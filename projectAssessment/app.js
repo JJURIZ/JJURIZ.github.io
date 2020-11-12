5451484d4b6dafe96b40ba7fb07eceee1eb20326
Jeremy Uriz <jeremyuriz@Jennifers-Air.attlocal.net>
2020-11-12 10:27:06 -0500

    [PA] Upload Project Assessment

---
+++ projectAssessment/app.js
@@ -0,0 +1,31 @@
+let currentValue = document.getElementById("current_value"),
+    incrementValue = document.getElementById("increment"),
+    decrementValue = document.getElementById("decrement"),
+    inputValue = document.getElementById("input_value");
+
+
+incrementValue.addEventListener("click", function(){
+   let num1 = convertCurrentValueToInt(currentValue);
+   let num2 = convertUserValueToInt(inputValue);
+    currentValue.textContent = num1 + num2;
+})
+
+decrementValue.addEventListener("click", function(){
+   let num1 = convertCurrentValueToInt(currentValue);
+   let num2 = convertUserValueToInt(inputValue);
+    currentValue.textContent = num1 - num2;
+})
+
+
+const convertCurrentValueToInt = (num) => {
+    let strValue = num.textContent;
+    let intValue = parseInt(strValue);  
+    return intValue;
+}
+
+const convertUserValueToInt = (num) => {
+    let strValue = num.value;
+    let intValue = parseInt(strValue);    
+    return intValue;
+}
+
